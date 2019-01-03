d2075c541f1c0e96fe0dd7cc28c30a8338c86978
neeleshjaiswal <neelesh.jaiswal08@gmail.com>
2019-01-03 23:27:31 +0530

    style changes

---
+++ Fourth-App/src/app/server/server.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core'
+
+@Component({
+       selector: 'server-component',
+       templateUrl: './server.component.html'
+       
+    })
+
+export class ServerComponent{
+    
+
+}
